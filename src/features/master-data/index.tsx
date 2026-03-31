af5c54a243f223b1a7d94e7cb730b8d6560d3756
hamdan-devsinc <hamdan.sajid@devsinc.com>
2026-03-31 21:07:27 +0500

    header added

--- src/features/master-data/index.tsx
+++ src/features/master-data/index.tsx
@@ -1,7 +1,15 @@
+import { PageHeader } from '@/components/layouts/page-header';
+
 export const MasterDataGrid = () => {
     return(
-        <div>
-            yooooos
+        <div className="min-h-screen">
+            <PageHeader
+                preTitle="MASTER DATA"
+                title="Master Data Management"
+                description="Manage departments, categories, suppliers, and reference data in one place."
+            />
+
+            
         </div>
     )
 }
